fa0dc08ba36deeed665bfeb27da14f8173928507
martmartalb <martmartalb@gmail.com>
2023-05-22 18:21:33 +0200

    ADDED test_Weights skeleton

---
+++ src/vhdl/test/test_Weights.vhd
@@ -0,0 +1,22 @@
+library ieee ;
+use ieee.std_logic_1164.all ;
+use ieee.numeric_std.all ;
+
+entity test_Weights is
+--   port (
+--     clock
+--   ) ;
+end test_Weights ; 
+
+architecture arch of test_Weights is
+
+component Bias 
+    Generic ( 
+
+    };
+    Port (
+
+    );
+end component;
+
+end architecture ;
