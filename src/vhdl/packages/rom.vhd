8f332e8a94c93d0a882428039bb2adc6722b8410
martmartalb <martmartalb@gmail.com>
2023-05-23 17:41:30 +0200

    SOLVED Bug in rom package and FIXED behavioral test.

--- src/vhdl/packages/rom.vhd
+++ src/vhdl/packages/rom.vhd
@@ -1,33 +1,33 @@
-library ieee; 
+library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use IEEE.std_logic_textio.ALL;
+use IEEE.std_logic_textio.all;
 
 library STD;
 use std.textio.all;
 
 package rom is
-    
-    type rom_type is array (natural range <>) of std_logic_vector;
-    impure function read_rom_file(filename : in string; data_length : in natural; rom_size : in natural) return rom_type;
-                            
+
+	type rom_type is array (natural range <>) of std_logic_vector;
+	impure function read_rom_file(filename : in string; data_length : in natural; rom_size : in natural) return rom_type;
+
 end package;
 
 package body rom is
-    
-    impure function read_rom_file(filename : in string; data_length : in natural; rom_size : in natural) return rom_type is
-		file rf : text open read_mode is filename;
-		variable v_l : line;
-		variable v_i : bit_vector(data_length-1 downto 0); 
-		variable v_rom  : rom_type(0 to rom_size)((data_length -1) downto 0);
+
+	impure function read_rom_file(filename : in string; data_length : in natural; rom_size : in natural) return rom_type is
+		file rf        : text open read_mode is filename;
+		variable v_l   : line;
+		variable v_i   : bit_vector(data_length - 1 downto 0);
+		variable v_rom : rom_type(0 to (rom_size - 1))((data_length - 1) downto 0);
 	begin
-		for i in 0 to rom_size loop 
+		for i in 0 to (rom_size - 1) loop
 			assert false report integer'image(data_length) severity NOTE;
 			readline (rf, v_l);
 			read (v_l, v_i);
-			v_rom(i) :=  to_stdlogicvector(v_i);
+			v_rom(i) := to_stdlogicvector(v_i);
 		end loop;
 		return v_rom;
-	end function; 
+	end function;
 
 end rom;
--- src/vhdl/params/Weights.vhd
+++ src/vhdl/params/Weights.vhd
@@ -1,39 +1,39 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
-use IEEE.std_logic_textio.ALL;
+use IEEE.std_logic_textio.all;
 
 library perceptron;
 use perceptron.rom.all;
 
 entity Weights is
 
-    Generic (
-        nbits_weights : natural := 3;
-        layer : natural := 1;
-        neuron : integer := 0;
-        size_prev_layer : natural := 100
-	);    
-    Port (
-    	clock : in STD_LOGIC;
-    	enable : in STD_LOGIC;
-    	weight_idx : in integer range 0 to size_prev_layer-1;
-    	dout : out signed (nbits_weights-1 downto 0)
+	generic (
+		nbits_weights   : natural := 4;
+		layer           : natural := 1;
+		neuron          : integer := 0;
+		size_prev_layer : natural := 4
 	);
-           
+	port (
+		clk        : in std_logic;
+		enable     : in std_logic;
+		weight_idx : in integer range 0 to size_prev_layer - 1;
+		dout       : out signed (nbits_weights - 1 downto 0)
+	);
+
 end Weights;
 
 architecture Behavioral of Weights is
 
-constant rom_size : natural := size_prev_layer;
---constant file_name : string := "MLP_weights" & "_" & integer'image(layer) & "_" & integer'image(neuron) & ".txt";
-constant MLP_weights : rom_type(0 to (rom_size-1))((nbits_weights-1) downto 0) := read_rom_file("weights.dat", nbits_weights, rom_size);
+	constant rom_size : natural := size_prev_layer;
+	--constant file_name : string := "MLP_weights" & "_" & integer'image(layer) & "_" & integer'image(neuron) & ".txt";
+	constant MLP_weights : rom_type(0 to (rom_size - 1))((nbits_weights - 1) downto 0) := read_rom_file("weights.dat", nbits_weights, rom_size);
 
 begin
- 	
- 	process(clock) is
+
+	process (clk) is
 	begin
-		if(rising_edge(clock) and enable = '1') then
+		if (rising_edge(clk) and enable = '1') then
 			dout <= signed(MLP_weights(weight_idx));
 		end if;
 	end process;
