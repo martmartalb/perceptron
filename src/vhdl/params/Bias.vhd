fae1960c1cde7d8108b246a035e544c70059eede
martmartalb <martmartalb@gmail.com>
2023-05-25 08:59:08 +0200

    REFACTORED Bias.vhd

--- src/vhdl/params/Bias.vhd
+++ src/vhdl/params/Bias.vhd
@@ -1,54 +1,38 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
-use IEEE.std_logic_textio.ALL;
+use IEEE.std_logic_textio.all;
 
-library STD;
-use std.textio.all;
+library perceptron;
+use perceptron.rom.all;
 
 entity Bias is
 
-  Generic (
-           n_bits_bias : natural := 3;
-           layer : natural := 1;
-           neuron : integer := 0
-           );
-           
-   Port (  
-           dout : out signed (n_bits_bias-1 downto 0));
-           
+    generic (
+        nbits_bias : natural := 4;
+        layer      : natural := 1;
+        neuron     : integer := 0
+    );
+    port (
+        clk    : in std_logic;
+        enable : in std_logic;
+        dout   : out signed (nbits_bias - 1 downto 0)
+    );
+
 end Bias;
 
 architecture Behavioral of Bias is
 
-constant size_rom : natural := 0;
+    constant rom_size : natural := 1;
 
-type rom_type is array (0 to size_rom) of signed (n_bits_bias-1 downto 0); 
-
-impure function bias_rom(rfn : in string) return rom_type is
-    file rf : text open read_mode is rfn;
-    variable v_l : line;
-    variable v_i : bit_vector(n_bits_bias-1 downto 0); 
-    variable v_rom  : rom_type;
-begin
-    for i in 0 to size_rom loop 
-        assert false report integer'image(n_bits_bias) severity NOTE;
-        readline (rf, v_l);
-        read (v_l, v_i);
-        v_rom(i) :=  signed(to_stdlogicvector(v_i));
-    end loop;
-    return v_rom;
-end function; 
-
-function create_file_name (neuron : in integer) return string is
-begin
-    return "MLP_Bias" & "_" & integer'image(layer) & "_" &  integer'image(neuron) & ".txt";
-end function;
-
-constant MLP_Bias : rom_type := bias_rom(create_file_name(neuron));
-
+    constant MLP_Bias : rom_type(0 to (rom_size - 1))((nbits_bias - 1) downto 0) := read_rom_file("bias.dat", nbits_bias, rom_size);
 begin
 
-dout <= MLP_Bias(0);
+    process (clk) is
+    begin
+        if (rising_edge(clk) and enable = '1') then
+            dout <= signed(MLP_Bias(0));
+        end if;
+    end process;
 
 end Behavioral;
