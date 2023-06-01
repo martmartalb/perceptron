9bcc584e630dae268c311fb89b6a49212eaea36f
martmartalb <martmartalb@gmail.com>
2023-06-01 12:22:26 +0200

    ADDED bus package to create type bus_array

---
+++ src/vhdl/packages/bus.vhd
@@ -0,0 +1,11 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package bus_pkg is
+    type bus_array is array(natural range <>) of signed;
+end package;
+
+package body bus_pkg is
+
+end bus_pkg;
